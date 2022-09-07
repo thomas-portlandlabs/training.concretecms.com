0b17516a1208a726fafe9f9cd83f6e532a41803b
Thomas Samson <thomas@portlandlabs.com>
2022-09-07 14:46:50 -0700

    unique codedeploy directories to prevent conflicts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 
-tempdir=/tmp/codedeployupload
+tempdir=/tmp/training/codedeployupload
 
 [[ -d $tempdir ]] && rm -r $tempdir
 mkdir -p $tempdir
 
+mkdir /tmp/$DEPLOYMENT_ID
+
+echo "export tempdir=\"$tempdir\"" > "/tmp/$DEPLOYMENT_ID/.cdvariables";
+
 if [ "$APPLICATION_NAME" == "training.stage.concretecms.com" ]
 then
-  echo "export projectdir=\"/home/forge/training.stage.concretecms.com\"" > "/tmp/.cdvariables";
-  echo "export deploydir=\"/home/forge/training.stage.concretecms.com/releases/$DEPLOYMENT_ID\"" >> "/tmp/.cdvariables";
+  echo "export projectdir=\"/home/forge/training.stage.concretecms.com\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
+  echo "export deploydir=\"/home/forge/training.stage.concretecms.com/releases/$DEPLOYMENT_ID\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
 elif [ "$APPLICATION_NAME" == "training.concretecms.com" ]
 then
-  echo "export projectdir=\"/home/forge/training.concretecms.com\"" > "/tmp/.cdvariables";
-  echo "export deploydir=\"/home/forge/training.concretecms.com/releases/$DEPLOYMENT_ID\"" >> "/tmp/.cdvariables";
+  echo "export projectdir=\"/home/forge/training.concretecms.com\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
+  echo "export deploydir=\"/home/forge/training.concretecms.com/releases/$DEPLOYMENT_ID\"" >> "/tmp/$DEPLOYMENT_ID/.cdvariables";
 fi
